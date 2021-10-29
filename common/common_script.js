48f03361c37aa5c1569ba177b7da4ffa3407e37a
lisasiurina <44965813+lisasiurina@users.noreply.github.com>
2021-10-29 13:36:33 +0200

    Add files via upload

---
+++ common/common_script.js
@@ -0,0 +1,25 @@
+﻿jQuery(document).ready(function($) {
+
+//меню
+$('#navbar-toggler').click(function() {
+
+    $('#navbarNavAltMarkup').toggle("");
+    $('#navbarNavAltMarkup').toggleClass('mob-hide');
+
+    if ($("#navbarNavAltMarkup").hasClass("mob-hide"))
+    $('#navbar-toggler img').attr('src', 'menu_img/icon_menu.svg');
+    else $('#navbar-toggler img').attr('src', 'menu_img/icon_close_menu.png');
+
+});
+
+
+$('a[href^="#"]').click( function(e)
+    {
+        var scroll_el = $(this).attr('href');
+        if ($(scroll_el).length != 0)
+        $('html, body').animate({ scrollTop: $(scroll_el).offset().top }, 500);
+        e.preventDefault();
+    });
+
+
+});
